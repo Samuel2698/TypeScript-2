05b506862f3320a5b6c77ce0facb3f86927b5075
Samuel Velasco <samuelvelasco2698@gmail.com>
2022-12-13 16:25:21 +0100

    Add files via upload

---
+++ variableMe.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var greetings = "Hello Samuel";
+var mynum = 6;
+greetings.toLowerCase();
+console.log(greetings);
+//* number
+var userId = 334455.3;
+userId.toFixed();
+//* boolean
+var isLoggedIn = false;
---
+++ variableMe.ts
@@ -0,0 +1,28 @@
+let greetings: string = "Hello Samuel";
+
+let mynum = 6;
+
+greetings.toLowerCase();
+console.log(greetings);
+
+//* number
+
+let userId = 334455.3;
+
+userId.toFixed();
+
+//* boolean
+
+let isLoggedIn: boolean = false;
+
+//* any
+
+let hero: string;
+
+function getHero() {
+  return "Thor";
+}
+
+hero = getHero();
+
+export {};
