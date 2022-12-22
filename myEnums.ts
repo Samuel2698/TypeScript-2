b7fd2a438f646499fdb572c0ad91fc943ad64300
Samuel Velasco <samuelvelasco2698@gmail.com>
2022-12-22 09:02:45 +0100

    Add files via upload

---
+++ myEnums.ts
@@ -0,0 +1,15 @@
+// enum SeatChoice {
+//   AISLE = 10,
+//   MIDDLE=22,
+//   WINDOW,
+//   FOURTH=44,
+// }
+
+const enum SeatChoice {
+  AISLE = "aisle",
+  MIDDLE = 3,
+  WINDOW,
+  FOURTH,
+}
+
+const hcSeat = SeatChoice.AISLE;
